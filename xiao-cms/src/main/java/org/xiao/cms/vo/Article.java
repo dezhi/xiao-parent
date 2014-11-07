b8c73154f9385bab908b6eb50387f7db8b32e2b2
xiaocui <dezhi@live.cn>
2014-11-07 15:13:48 +0800

    这是第二版本的文章

--- xiao-cms/src/main/java/org/xiao/cms/vo/Article.java
+++ xiao-cms/src/main/java/org/xiao/cms/vo/Article.java
@@ -13,4 +13,60 @@ public class Article {
     private Date created;
     private Date edited;
     private Date published;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public Date getEdited() {
+        return edited;
+    }
+
+    public void setEdited(Date edited) {
+        this.edited = edited;
+    }
+
+    public Date getPublished() {
+        return published;
+    }
+
+    public void setPublished(Date published) {
+        this.published = published;
+    }
 }
