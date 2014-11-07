3154cc9d5d7ff5a01eaab91af25c2c62fd085647
xiaocui <dezhi@live.cn>
2014-11-07 15:16:20 +0800

    这是第三个版本的文章

--- xiao-cms/src/main/java/org/xiao/cms/vo/Article.java
+++ xiao-cms/src/main/java/org/xiao/cms/vo/Article.java
@@ -69,4 +69,34 @@ public class Article {
     public void setPublished(Date published) {
         this.published = published;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Article article = (Article) o;
+
+        if (id != article.id) return false;
+        if (content != null ? !content.equals(article.content) : article.content != null) return false;
+        if (created != null ? !created.equals(article.created) : article.created != null) return false;
+        if (edited != null ? !edited.equals(article.edited) : article.edited != null) return false;
+        if (name != null ? !name.equals(article.name) : article.name != null) return false;
+        if (published != null ? !published.equals(article.published) : article.published != null) return false;
+        if (summary != null ? !summary.equals(article.summary) : article.summary != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (summary != null ? summary.hashCode() : 0);
+        result = 31 * result + (content != null ? content.hashCode() : 0);
+        result = 31 * result + (created != null ? created.hashCode() : 0);
+        result = 31 * result + (edited != null ? edited.hashCode() : 0);
+        result = 31 * result + (published != null ? published.hashCode() : 0);
+        return result;
+    }
 }
