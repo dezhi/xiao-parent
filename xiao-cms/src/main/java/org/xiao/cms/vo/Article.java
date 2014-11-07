347843ed265de37f128e56b977b5903495e2d6c0
xiaocui <dezhi@live.cn>
2014-11-07 15:12:13 +0800

    这是第一版本的文章

---
+++ xiao-cms/src/main/java/org/xiao/cms/vo/Article.java
@@ -0,0 +1,16 @@
+package org.xiao.cms.vo;
+
+import java.util.Date;
+
+/**
+ * Created by Administrator on 2014/11/7.
+ */
+public class Article {
+    private int id;
+    private String name;
+    private String summary;
+    private String content;
+    private Date created;
+    private Date edited;
+    private Date published;
+}
