b3fe0af906e1c12e630510ff7e7eb847464bc02c
xiaocui <dezhi@live.cn>
2014-11-09 10:23:43 +0800

    添加三个项目，orm，blog，cms

---
+++ xiao-cms/src/main/java/com/xiao/cms/vo/Comment.java
@@ -0,0 +1,41 @@
+package com.xiao.cms.vo;
+
+public class Comment {
+	private int id;
+	private String name;
+	private String created;
+	private int userId;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCreated() {
+		return created;
+	}
+
+	public void setCreated(String created) {
+		this.created = created;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+}
